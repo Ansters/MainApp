802096ec86d87080a0198f3b56987870395db294
Adisorn Sripakpaisit <adisorn.akr@gmail.com>
2022-04-20 20:13:44 +0700

    test use class from sub module

--- app/src/main/java/me/ansters/mainapp/MainActivity.kt
+++ app/src/main/java/me/ansters/mainapp/MainActivity.kt
@@ -2,10 +2,13 @@ package me.ansters.mainapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import me.ansters.share.core.Constants
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.tv_text).text = Constants.MODULE_NAME
     }
 }
